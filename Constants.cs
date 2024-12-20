d57c92f31a11de3d8cf406ef43dc4cf9d3aee43d
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-12-20 05:06:54 +0300

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Constants
+{
+    class Program // Main metodunu içeren bir sınıf
+    {
+        static void Main(string[] args) // Programın giriş noktası
+        {
+            const double pi = Math.PI;
+            Console.WriteLine("PI = " + pi);
+            Console.ReadKey();
+        }
+    }
+}
