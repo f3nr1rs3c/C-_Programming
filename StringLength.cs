36e8cc3988eecc16325712708bf59ab92369b71c
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-12-20 05:22:27 +0300

    Create StringLength.cs

---
+++ StringLength.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace StringLength
+{
+    class Program // Main metodunu içeren bir sınıf
+    {
+        static void Main(string[] args) // Programın giriş noktası
+        {
+            String text = "GoTouchGrass";
+            Console.WriteLine(text.Length);
+            Console.ReadKey();
+        }
+    }
+}
