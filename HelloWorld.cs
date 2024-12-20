86ee82b9df0703a1cf2c3fdc29f2a5f507ee62a4
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-12-20 04:56:43 +0300

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ");
+            Console.WriteLine(" World!");
+            Console.WriteLine("I'm starting a C# Tutorial.");
+            Console.ReadKey();
+        }
+    }
+}
